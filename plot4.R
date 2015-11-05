4abde4d40fd400ca93c2422511d6904e82cfc4eb
Haseeb <muhammadhaseebahmad@yahoo.com>
2015-11-05 18:53:26 +0500

    Project 1 Update 7

--- plot4.R
+++ plot4.R
@@ -16,17 +16,24 @@ power_df <<- mutate(power_df, DateTime = ymd_hms(paste(dmy(power_df$Date),
                                                        power_df$Time)))
 
 png('./plot4.png', width = 480, height = 480)
+
+
 par(bg = 'transparent')
 par(mfcol = c(2, 2))
+
 plot(power_df$DateTime, power_df$Global_active_power, type = 'l', xlab = '',
      ylab = 'Global Active Power')
-plot(power_df$DateTime, power_df$Sub_metering_1, col = 'black', type = 'l', xlab='')
+
+plot(power_df$DateTime, power_df$Sub_metering_1, col = 'black', type = 'l', 
+     xlab='', ylab = 'Energy sub metering')
 lines(power_df$DateTime, power_df$Sub_metering_2, col = 'red')
 lines(power_df$DateTime, power_df$Sub_metering_3, col = 'blue')
 legend('topright', lty = 1, col = c('black', 'red', 'blue'), 
        legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
+
 plot(power_df$DateTime, power_df$Voltage, type ='l', xlab = 'datetime', 
      ylab = 'Voltage')
+
 plot(power_df$DateTime, power_df$Global_reactive_power, type = 'l',
      xlab = 'datetime', ylab = 'Global_reactive_power')
 dev.off()
